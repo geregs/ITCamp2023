cb72f325966a22ec0306ee000e679fc04c38993a
geregs <geregs@gmail.com>
2024-01-03 23:02:22 +0100

    Operacije

---
+++ geregHTML03/index.js
@@ -0,0 +1,38 @@
+
+
+//function brojevi(broj, broj2){
+//    broj=Number(prompt(`unesi prvi broj`))
+//   broj2=Number(prompt(`unesi drugi broj`))
+//   zbir=broj+broj2
+//   console.log(`Zbir dva broja je ${zbir}`)
+//}
+//brojevi()
+
+num1 = Number(prompt("Napiši prvi broj"))
+num2 = Number(prompt("Napiši drugi broj"))
+operator = prompt("Napiši jedan od operatora i to: +, -, * ili /")
+function chkOperator(){
+    switch(operator){
+        case "*":
+            text1="Proizvod"
+            result=num1*num2
+            break;
+        case "/":
+            text1="Količnik"
+            result=num1/num2
+            break;
+        case "+":
+            text1="Zbir"
+            result=num1+num2
+            break;
+        case "-":
+            text1="Razlika"
+            result=num1-num2
+            break;
+        default:
+            operator = prompt("LEPO TI PIŠE da napišeš jedan od operatora i to: +, -, * ili /") 
+            chkOperator()
+    }
+}
+chkOperator()
+alert(`${text1} ovih brojeva je ${result}.`)
